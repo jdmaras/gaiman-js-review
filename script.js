83d8da1e5df8a36440b7241c237f6b605905713e
John Maras <sportsballenthusiast@gmail.com>
2022-05-09 11:49:03 -0500

    fixed a bunch of the errors with wrongly named expressions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log(`I am in script.js`);
 
-let myName = 'John';
-console.log('My name is', myName);
+let myName = "John";
+console.log("My name is ", myName);
 // console will log out the sentence My name is and then the variable will fill in what it's equal to
 /*
 let -- a KEYWORD, we are about to declare a variable
@@ -13,17 +13,17 @@ myName -- the name of the variable
 (the string) 'John' -- the value
 */
 // Use camelCase
-let lastName = 'Maras';
+let myLastName = "Maras";
 
 //Other kinds of casing:
 // snake_case
-let my_last_name= 'Maras';
+//let my_last_name = 'Maras';
 
 //kebob-case
-let my-last-name= 'Maras';
+//let my-last-name = 'Maras';
 
 //SCREAMING_SNAKE_CASE
-let MY_LAST_NAME = 'Maras';
+//let MY_LAST_NAME = 'Maras';
 
 /*
 Expressions
@@ -31,11 +31,20 @@ Expressions
 
 */
 
-console.log('My name is ' + firsName '' + myLastName);
-          //  ^ this is an expression!
-          // console.log (`My name is John Maras`);
+console.log("My name is ", myName + " " + myLastName);
+//  ^ this is an expression!
+// console.log (`My name is John Maras`);
 function add(n1, n2) {
-    return n1 + n2;
+  return n1 + n2;
 }
 let result = add(5, 7);
 // let result = 12
+//if (8 > 7) {
+// if (false)
+// ....
+//}
+
+let isJamesBond = myName + " " + myLastName === `James Bond`;
+// let isJamesBond = 'John Maras' === 'James Bond';
+// let isJamesBond = false;
+console.log("isJamesBond", isJamesBond);
