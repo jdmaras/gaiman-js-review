641a4f8a9651a8ad33558ecdb7c7ef3176092a73
John Maras <sportsballenthusiast@gmail.com>
2022-05-09 11:33:26 -0500

    talked about different cases

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log(`I am in script.js`);
+
+let myName = 'John';
+console.log('My name is', myName);
+// console will log out the sentence My name is and then the variable will fill in what it's equal to
+/*
+let -- a KEYWORD, we are about to declare a variable
+
+myName -- the name of the variable
+
+= -- the equal sign is the assignment operator
+
+(the string) 'John' -- the value
+*/
+// Use camelCase
+let lastName = 'Maras';
+
+//Other kinds of casing:
+// snake_case
+let my_last_name= 'Maras';
+
+//kebob-case
+let my-last-name= 'Maras';
+
+//SCREAMING_SNAKE_CASE
+let MY_LAST_NAME = 'Maras';
